6ea2c7f82a4246a4e1136092d56ea48d70881ec8
sara <sarah.h899@outlook.com>
2021-03-15 17:28:51 +0300

    added some change to body

--- hello.php
+++ hello.php
@@ -4,8 +4,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+body{
+   height:600px;
+   color:red;
+
+}
+  
+</style>
 <body>
-   <h1>hello</h1> 
+    <h1>hello</h1>
 </body>
 </html>
 
