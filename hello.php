7677050b1da279e3123d5b13070b14bf42b0984d
sara <sarah.h899@outlook.com>
2021-03-16 17:13:51 +0300

    php

--- hello.php
+++ hello.php
@@ -4,19 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<style>
-body{
-   height:600px;
-min-height:600px;
-   color:red;
-
-}
-  
-</style>
 <body>
-<p>welcome</p>
-   <h1>hello</h1>
-    <h1>hello</h1>
+   <p><?php
+echo 'Hello World';
+?></p> 
 </body>
 </html>
+
 
